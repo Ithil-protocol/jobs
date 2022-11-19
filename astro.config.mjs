54b63ed9c93ae239b1d76c69be7fb4cb144bd58c
OxMarco <>
2022-11-19 01:06:25 +0100

    try absolute paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,9 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-paper.pages.dev/",
+  site: "https://jobs.ithil.fi/",
+  base: "https://jobs.ithil.fi/",
+  output: "static",
   integrations: [
     tailwind({
       config: {
